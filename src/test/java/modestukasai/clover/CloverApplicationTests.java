787163a8d0b45f13be77f68346c3e428d8713755
Modestas Kažinauskas <modestas.kazinauskas@4finance.com>
2019-08-16 16:19:04 +0300

    Inital commit

---
+++ src/test/java/modestukasai/clover/CloverApplicationTests.java
@@ -0,0 +1,16 @@
+package modestukasai.clover;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CloverApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
